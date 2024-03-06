6c6e19ad060cc05f866fe0a3fa0f4af34dc24b92
Mudith Milinda <137484889+MudithMilinda@users.noreply.github.com>
2024-03-06 13:50:47 +0530

    C# Lab session

---
+++ Lab Sheet 02/Q03/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q03
+{
+    class TemperatureTracker
+    {
+        private double[] dailyTemperatures;
+        
+        public TemperatureTracker()
+        {
+            dailyTemperatures = new double[7];
+        }
+
+        public void InputTemperatures()
+        {
+            for (int i = 0; i < dailyTemperatures.Length; i++)
+            {
+                Console.WriteLine($"Enter temperature for day {i + 1}: ");
+                double temperture = Convert.ToDouble(Console.ReadLine());
+                dailyTemperatures[i] = temperture;
+            }
+        }
+
+        public void WeeklyTemperature()
+        {
+            Console.WriteLine("Weekly Temperatures: ");
+            for (int i = 0; i < dailyTemperatures.Length; i++)
+            {
+                Console.WriteLine($"Day {i + 1}: {dailyTemperatures[i]}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureTracker tracker = new TemperatureTracker();
+            tracker.InputTemperatures();
+            tracker.WeeklyTemperature();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab Sheet 02/Q04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q04
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+
+        public Product(string productName, double price)
+        {
+            ProductName = productName;
+            Price = price;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Laptop", 1100.00);
+            Product product2 = new Product("Keyboard", 30.00);
+            Product product3 = new Product("Motherboard", 100.00);
+
+            Console.WriteLine($"Product Name: {product1.ProductName}" + $"Price: {product1.Price}");
+            Console.WriteLine($"Product Name: {product2.ProductName}" + $"Price: {product2.Price}");
+            Console.WriteLine($"Product Name: {product3.ProductName}" + $"Price: {product3.Price}");
+
+            Console.ReadLine();
+        }
+    }
+}
